382676483dd06802ab13650e904cba710f46b32f
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-09-13 14:09:21 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/tellybots_4u"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/tellybots_4u'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/sahaynitin/tellyfilterbotv2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/tellybots_support')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
